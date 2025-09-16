70669400891cf6ce8d4b9eaad61046700b79cd82
Nobody12here <uraja01313@gmail.com>
2025-09-16 08:56:21 +0500

    refactor: enhance login form and authentication logic

--- account/views.py
+++ account/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpRequest
+from django.contrib.auth import authenticate
 
-
-def login(request):
-    return render(request, "account/login.html")
+def login(request:HttpRequest):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password =  request.POST.get("password")
+        user = authenticate(email="test@gmail.com",password="test123")
+        print(email,password)
+        print(user)
+        return render(request, "account/login.html")
+    else:
+        return render(request, "account/login.html")
