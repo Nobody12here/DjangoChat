d4221db955a2eb229bedee6a48a63eadde773797
Nobody12here <uraja01313@gmail.com>
2025-09-20 12:09:37 +0500

    added some email form related stuff

--- account/views.py
+++ account/views.py
@@ -6,9 +6,11 @@ def login(request:HttpRequest):
     if request.method == "POST":
         email = request.POST.get("email")
         password =  request.POST.get("password")
-        user = authenticate(email="test@gmail.com",password="test123")
-        print(email,password)
-        print(user)
-        return render(request, "account/login.html")
+        user = authenticate(username=email,password=password)
+        message = "Logged in sucessfully"
+        if not user:
+            message = "Invalid credentials"
+        context = {"message":message}
+        return render(request, "account/login.html",context)
     else:
         return render(request, "account/login.html")
