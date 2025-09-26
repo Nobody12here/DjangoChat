c51d46097227a78bbb7a0b82e0f5ea2b2046d1a6
Nobody12here <uraja01313@gmail.com>
2025-09-26 11:24:11 +0500

    Add Room and Message models with migrations; update .gitignore

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    """Model definition for Room."""
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    admin = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="admin_rooms",
+    )  # room owner
+    is_private = models.BooleanField(default=False)
+    members = models.ManyToManyField(User, blank=True, related_name="chat_rooms")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Room."""
+
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+
+    def __str__(self):
+        return self.name
+
+    def transfer_admin(self):
+        if (
+            self.admin not in self.members.all()
+        ):  # if the current admin is not in the members list (admin has left the room)
+            new_admin = self.members.first()
+            if new_admin:
+                self.admin = new_admin
+                self.save()
+
+
+class Message(models.Model):
+    """Model definition for Message."""
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    text = models.CharField(max_length=4096)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Message."""
+
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
+
+    def __str__(self):
+        """Unicode representation of Message."""
+        self.sender.last_name
