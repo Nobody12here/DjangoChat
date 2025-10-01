91a7da686cfdbde46d5eb955e3dd023214f8af11
Nobody12here <uraja01313@gmail.com>
2025-10-01 13:10:33 +0500

    Add list rooms template and update views and URLs for room management

--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,11 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [path("create-room/", views.create_room, name="create-room")]
+urlpatterns = [
+    path("create-room/", views.create_room, name="create-room"),
+    path(
+        "list-rooms/",
+        views.list_rooms,
+        name="list-rooms",
+    ),
+]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Message, Room
 from .forms import ChatRoomForm
@@ -7,11 +7,22 @@ from .forms import ChatRoomForm
 
 @login_required(login_url="/account")
 def create_room(request: HttpRequest):
-    room_form = ChatRoomForm()
     if request.method == "POST":
         room_form = ChatRoomForm(request.POST)
         if room_form.is_valid():
-            room_form.save()
-            return render(request, "chat/create_room.html")
+            chat_room: Room = room_form.save(commit=False)
+            chat_room.admin = request.user
+            chat_room.save()
+            return redirect("list-rooms")
+    else:
+        room_form = ChatRoomForm()
+
     context = {"room_form": room_form}
     return render(request, "chat/create_room.html", context)
+
+
+@login_required(login_url="/account")
+def list_rooms(request: HttpRequest):
+    rooms = Room.objects.filter(admin=request.user)
+    context = {"rooms": rooms}
+    return render(request, "chat/list_rooms.html", context)
